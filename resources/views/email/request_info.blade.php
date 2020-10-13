7df3c66f869697d8d5433716e81526ccc9396567
Jordan Anderson <jordanmand@gmail.com>
2020-10-13 05:35:39 -0700

    More request info email changes

--- resources/views/email/request_info.blade.php
+++ resources/views/email/request_info.blade.php
@@ -1,22 +1,26 @@
 <html>
 <body style="background-image: url({{ asset('img/bg/sky3.jpg') }})">
-	<img src="{{ asset('img/starlogo-small.png') }}" />
-	<div style="background-color: #fff; border-radius: 7px; margin: 10px; padding: 5px 15px 15px;">
-		<p>Jim,<br> A customer has submitted a request for more information about the Interactive Curriculum! Here are the details:</p>
-		<p>
-			<label><strong>Name:</strong></label> {{ $customer->name }}<br>
-			<label><strong>Email:</strong></label> {{ $customer->email }}<br>
-			<label><strong>Home Phone:</strong></label> {{ $customer->phone_home }}<br>
-			
-			@if ($customer->child_name)
-				<label><strong>Child's Name:</strong></label> {{ $customer->child_name }}<br>
-			@endif
+	<div style="padding: 15px; margin: 0 auto;">
+		<img style="display: block;" src="{{ asset('img/starlogo-small.png') }}" />
+		<div style="background-color: #fff; border-radius: 7px; margin: 10px; padding: 5px 15px 15px; display: inline-block;">
+			<p>Jim,<br> A customer has submitted a request for more information about the Interactive Curriculum! Here are the details:</p>
+			<p>
+				<label><strong>Name:</strong></label> {{ $customer->name }}<br>
+				<label><strong>Email:</strong></label> {{ $customer->email }}<br>
+				<label><strong>Home Phone:</strong></label> {{ $customer->phone_home }}<br>
+				
+				@if ($customer->child_name)
+					<label><strong>Child's Name:</strong></label> {{ $customer->child_name }}<br>
+				@endif
 
-			@if ($customer->fullGrade)
-				<label><strong>Child's Grade:</strong></label> {{ $customer->fullGrade->name }}<br>
-			@endif
-		</p>
-		<p><a href="{{ route('admin.customer', ['customer' => $customer->id]) }}" target="_blank">Click here to manage this customer in the Admin CRM</a></p>
+				@if ($customer->fullGrade)
+					<label><strong>Child's Grade:</strong></label> {{ $customer->fullGrade->name }}<br>
+				@endif
+			</p>
+			<p>
+				<a href="{{ route('admin.customer', ['customer' => $customer->id]) }}" target="_blank">Click here to manage this customer in the Admin CRM</a>
+			</p>
+		</div>
 	</div>
 </body>
 </html>
